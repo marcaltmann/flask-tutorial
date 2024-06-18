7f98cd3c734c8ab794567d9d64f1010082c8d734
Marc Altmann <marcaltmann@posteo.de>
2024-06-18 18:30:34 +0200

    Add blog code

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -31,9 +31,16 @@ def create_app(test_config=None):
         return f"Hello {a}, World!"
 
     from . import db
+
     db.init_app(app)
 
     from . import auth
+
     app.register_blueprint(auth.bp)
+
+    from . import blog
+
+    app.register_blueprint(blog.bp)
+    app.add_url_rule("/", endpoint="index")
 
     return app
